ea7ba71d17009328b12222eb482a4dd71171d9b0
AsukA <156112057+ZigiWin@users.noreply.github.com>
2025-05-27 00:22:06 +0300

    Update conect.php

--- public/conect.php
+++ public/conect.php
@@ -1,7 +1,19 @@
 <?php
-$conn = new mysqli('MySQL-8.2', 'root', '', 'CertiCraft');
+$host = 'dpg-d0qdjlodl3ps73esect0-a';
+$port = 5432;
+$dbname = 'certicraft';
+$user = 'admin';
+$password = 'HwR6Pv9v2QIRogC3NeH2s8hWT1lXSOJl';
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+
+try {
+    $pdo = new PDO($dsn, $user, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Включить выброс исключений при ошибках
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC // Удобный режим выборки
+    ]);
+    echo "Подключение к PostgreSQL через PDO успешно!";
+} catch (PDOException $e) {
+    die("Ошибка подключения: " . $e->getMessage());
 }
 ?>
